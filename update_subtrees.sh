c46cf486914dbe06291be1ab6d04fe64f4cb0a95
Holden Karau <holden@pigscanfly.ca>
2020-12-12 16:51:13 -0800

    Upadte the zero to jhub subtree

--- update_subtrees.sh
+++ update_subtrees.sh
@@ -1,3 +1,4 @@
 git subtree pull  --prefix=subrepos/scalingpythonml git@github.com:scalingpythonml/scalingpythonml.git master
 git subtree pull  --prefix=subrepos/dask-docker git@github.com:holdenk/dask-docker.git master
 git subtree pull  --prefix=subrepos/docker-stacks git@github.com:holdenk/docker-stacks.git master
+git subtree pull  --prefix=subrepos/zero-to-jupyterhub-k8s git@github.com:holdenk/zero-to-jupyterhub-k8s.git master
