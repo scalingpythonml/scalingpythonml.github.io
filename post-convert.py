223417a2c93e1ddbabf31cfab8911e4a13b0c006
Holden Karau <holden@pigscanfly.ca>
2020-12-20 12:14:30 -0800

    Add a script to convert the output from google markdown doohicky thing

---
+++ post-convert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description="Clean up asciidoc which has been exported using the Google doc asciidoc exporter")
+parser.add_argument('files', metavar='N', type=str, nargs='+',
+                    help='files to clean up')
+args = parser.parse_args()
+
+
+replacements = {r"\+(\Z|$)": '', "^== ":"= "}
+for filename in args.files:
+    with open(filename) as infile, open(filename, 'w') as outfile:
+        for line in infile:
+            for src, target in replacements.items():
+                line = re.sub(src, target, line)
+            outfile.write(line)
